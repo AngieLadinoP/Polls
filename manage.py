e39d2c17bad2d8996ffed8b7709982b9743609ca
Angie <agladinop@gmail.com>
2022-03-12 10:50:11 -0500

    heroku settings

--- manage.py
+++ manage.py
@@ -16,11 +16,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
